dd912d3b8fd37cd8b861a6f7548e6ca6350a13af
ChrisP223 <christiannaap662@gmail.com>
2026-04-10 18:31:40 +0300

    Update MPU_CODE.ino

--- MPU_CODE.ino
+++ MPU_CODE.ino
@@ -1,32 +1,41 @@
 #include <MPU6050_tockn.h>  
 #include <Wire.h> 
 
-
-MPU6050 mpu6050(Wire, 0.05, 0.95);//τα filter settings μπορούν να αλλάξουν
+MPU6050 mpu6050(Wire, 0.05, 0.95);
 
 void setup() {
   Serial.begin(9600);
   Wire.begin(); 
   mpu6050.begin(); 
-  delay(2000);//stabilize
-  mpu6050.calcGyroOffsets(true);//Calibrate offsets
+  delay(2000);
+  mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
-  mpu6050.update();  // Read and process sensor data
-  // Get orientation angles
+  mpu6050.update();//process sensor data
+
+ 
   float roll  = mpu6050.getAngleX();
   float pitch = mpu6050.getAngleY();
   float yaw   = mpu6050.getAngleZ();
-  //deadzone για να αποφύγουμε noise
-  if (abs(roll)  < 0.5) roll  = 0;
-  if (abs(pitch) < 0.5) pitch = 0;
-  if (abs(yaw)   < 0.5) yaw   = 0;
-  //data in csv format
-  Serial.print(roll);
-  Serial.print(",");
-  Serial.print(pitch);
-  Serial.print(",");
+
+  //RAW accel angles(noisy, no gyro, no drift)
+  float ax = mpu6050.getAccX();
+  float ay = mpu6050.getAccY();
+  float az = mpu6050.getAccZ();
+  float roll_raw  = atan2(ay, az)*180.0/PI;
+  float pitch_raw = atan2(-ax, sqrt(ay*ay+az*az))*180.0/PI;
+
+ //deadzone
+  if (abs(roll)< 0.5) roll = 0;
+  if (abs(pitch)< 0.5) pitch = 0;
+  if (abs(yaw)< 0.5) yaw = 0;
+
+  //CSV
+  Serial.print(roll_raw);   Serial.print(",");
+  Serial.print(pitch_raw);  Serial.print(",");
+  Serial.print(roll);       Serial.print(",");
+  Serial.print(pitch);      Serial.print(",");
   Serial.println(yaw);
 
   delay(50); 
