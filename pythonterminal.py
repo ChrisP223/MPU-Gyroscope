f23b91cdbc8a3924896c1873a361ba0e1c7a3368
ChrisP223 <christiannaap662@gmail.com>
2026-04-10 18:20:45 +0300

    Update pythonterminal.py

--- pythonterminal.py
+++ pythonterminal.py
@@ -1,18 +1,26 @@
-#ΟΔΗΓΙΕΣ:
-#1) ΤΡΕΧΟΥΜΕ ΣΤΟ TERMINAL ΑΦΟΥ ΤΡΕΧΕΙ ΤΟ ARDUINO
-#2)Ctrl-C για να σταματήσουμε το recording
-#3)Αφού σταματήσω να το τρέχω, τρέχω το visualizer.py για να δω το output
+# ΟΔΗΓΙΕΣ:
+# 1) ΤΡΕΧΟΥΜΕ ΣΤΟ TERMINAL ΑΦΟΥ ΤΡΕΧΕΙ ΤΟ ARDUINO
+# 2) Ctrl-C για να σταματήσουμε το recording
+# 3) Αφού σταματήσω να το τρέχω, τρέχω το visualizer.py για να δω το output
 
-
-python -c "
 import serial, time
-PORT = 'COM3'#port number
+
+PORT = 'COM3'
 ser = serial.Serial(PORT, 9600, timeout=1)
 print('press Ctrl+C to stop')
+
 with open('movement.txt', 'w') as f:
-    while True:
-        line = ser.readline().decode('utf-8').strip()
-        if ',' in line:
-            f.write(line + '\n')
-            print(line)
-"
+    f.write("roll_raw,pitch_raw,roll_filtered,pitch_filtered,yaw_filtered\n")
+    try:
+        while True:
+            line = ser.readline().decode('utf-8').strip()
+            if ',' in line:
+                parts = line.split(',')
+                if len(parts) == 5:
+                    f.write(line + '\n')
+                    print(line)
+                else:
+                    print(f"skippedline: {line}")
+    except KeyboardInterrupt:
+        print("\nData saved to movement.txt")
+        ser.close()
